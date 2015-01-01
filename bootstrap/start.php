4062a2b62bb1e7d1d29943ee5858c4ebbbd34911
Lisa Riggle <julisana@gmail.com>
2014-12-31 19:28:34 -0800

    Setting up the environment so that overrides can be put in app/config/local
     Changes to be committed:
    	modified:   bootstrap/start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,18 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+/*$env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array('dev'),
 
-));
+));*/
+$env = $app->detectEnvironment(function ()
+{
+	if (file_exists(__DIR__ . '/environment.php')) {
+		return require __DIR__ . '/environment.php';
+	}
+	return 'local';
+});
 
 /*
 |--------------------------------------------------------------------------
