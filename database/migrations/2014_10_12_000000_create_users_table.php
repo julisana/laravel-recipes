eac9870d2df12c5379d285a1b4f776bf0a1a1057
Lisa Riggle <julisana@gmail.com>
2015-09-06 09:51:44 -0500

    re-adding default migrations back to project.
     Changes to be committed:
    	renamed:    database/migrations/2015_09_06_010926_create_users_table.php -> database/migrations/2014_10_12_000000_create_users_table.php
    	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,16 +12,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function ($table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->string('username')->unique();
-            $table->string('password');
+            $table->string('password', 60);
             $table->string('email')->unique();
+
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
 
-            $table->string('remember_token')->nullable();
-
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('users');
     }
 }
