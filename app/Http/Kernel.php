611065db4acd23288fb83f57bed0e7801fa94d19
Lisa Riggle <julisana@gmail.com>
2015-09-06 10:32:22 -0500

    All admin URLs should use be on HTTPS, not HTTP. Adding a check to ensure we are using https. https://github.com/julisana/laravel-recipes/issues/5
     Changes to be committed:
    	modified:   app/Http/Controllers/HomeController.php
    	modified:   app/Http/Kernel.php
    	new file:   app/Http/Middleware/CheckHttps.php
    	modified:   app/Http/routes.php
    	modified:   app/Providers/RouteServiceProvider.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,5 +29,6 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'https' => \App\Http\Middleware\CheckHttps::class,
     ];
 }
