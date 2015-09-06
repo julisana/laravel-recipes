cd4379e7f196d8dae2e3a158e018c00870b26eef
Lisa Riggle <julisana@gmail.com>
2015-09-05 20:19:41 -0500

    Adding Homestead/Vagrant and basic database migrations. https://github.com/julisana/laravel-recipes/issues/2
     Changes to be committed:
    	modified:   .gitignore
    	new file:   Homestead.yaml
    	new file:   Vagrantfile
    	modified:   composer.json
    	new file:   database/migrations/2015_09_06_010926_create_users_table.php
    	new file:   database/migrations/2015_09_06_011032_create_recipes_table.php
    	new file:   database/migrations/2015_09_06_011420_create_ingredients_table.php
    	new file:   database/migrations/2015_09_06_011556_create_directions_table.php

---
+++ database/migrations/2015_09_06_011032_create_recipes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function($table)
+        {
+            $table->increments('id')->unsigned();
+
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('source')->nullable();
+            $table->string('source_url')->nullable();
+            $table->text('notes')->nullable();
+            $table->bigInteger('prep_time')->default(0)->unsigned();   //seconds
+            $table->bigInteger('cook_time')->default(0)->unsigned();   //seconds
+
+            $table->timestamps();
+            $table->integer('created_by')->unsigned();
+            $table->integer('modified_by')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
