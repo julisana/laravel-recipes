8329c892ba2ad104831e100868e66c7281fb73ac
Lisa Riggle <julisana@gmail.com>
2015-09-15 21:42:57 -0500

    Starting work on profile interfaces. https://github.com/julisana/laravel-recipes/issues/13
     Changes to be committed:
    	modified:   app/Http/Controllers/Controller.php
    	new file:   app/Http/Controllers/ProfileController.php
    	modified:   app/Http/routes.php
    	modified:   resources/views/layout.twig
    	new file:   resources/views/navigation.twig
    	new file:   resources/views/profile/index.twig
    	new file:   resources/views/profile/recipe.twig
    	new file:   resources/views/profile/recipes.twig

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Response;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return Response::view('profile.index');
+    }
+
+    public function recipes()
+    {
+        return Response::view('profile.recipes');
+    }
+
+    public function recipe($slug, $id)
+    {
+        return Response::view('profile.recipe');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,12 +13,16 @@
 
 Route::get('/', 'HomeController@index');
 
-Route::group(['prefix' => 'admin', 'middleware' => ['https', 'auth']], function () {
-
-});
-
 Route::group(['prefix' => 'recipes'], function() {
     Route::get('/', ['as' => 'recipes.index', 'uses' => 'RecipeController@index']);
-    Route::get('{recipeSlug}/{id}', [ 'as' => 'recipes.show', 'uses' => 'RecipeController@show' ])
+    Route::get('{recipeSlug}/{id}', ['as' => 'recipes.show', 'uses' => 'RecipeController@show'])
+        ->where('id', '[\d]+');
+});
+
+Route::group(['prefix' => 'profile'], function() {
+//Route::group(['prefix' => 'profile', 'middleware' => ['https', 'auth']], function() {
+    Route::get('/', ['as' => 'profile.index', 'uses' => 'ProfileController@index']);
+    Route::get('/recipes', ['as' => 'profile.recipes', 'uses' => 'ProfileController@recipes']);
+    Route::get('/{recipeSlug}/{id}', ['as' => 'profile.recipes.show', 'uses' => 'ProfileController@recipe'])
         ->where('id', '[\d]+');
 });
