c461c8515a39f3c5dabf5c7f6702f78f424f5c44
Lisa Riggle <julisana@gmail.com>
2015-09-19 12:37:56 -0500

    Updating the validation rules for user registration so they aren't forgotten later. https://github.com/julisana/laravel-recipes/issues/13
     Changes to be committed:
    	modified:   app/Http/Controllers/Auth/AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -45,9 +45,9 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'username' => 'required|max:255|unique:users',
             'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
+            'password' => 'required|confirmed|min:8',
         ]);
     }
 
@@ -60,7 +60,7 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
