23b417df9b1966fa2062d0ac9cc459000a001a0b
Lisa Riggle <julisana@gmail.com>
2019-01-16 18:55:23 -0600

    The last changes I think
     Changes to be committed:
    	modified:   app/Http/Controllers/Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,56 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var array
+     */
+    protected $context = [];
+
+    /**
+     * If we need to add an error message (or any other kind of message), pull the existing messages
+     * and add to it.
+     *
+     * @param string $type
+     * @param mixed  $value
+     *
+     * @return Controller
+     */
+    protected function addMessage( $type, $value )
+    {
+        $messages = view()->shared( 'messages', [] );
+        if ( empty( $messages ) ) {
+            $messages = array_get( $this->context, 'messages', [] );
+        }
+
+        //Errors is the only 'message' that's got a plural name, let's check for the singular just in case.
+        if ( $type == 'error' ) {
+            $type = 'errors';
+        }
+
+        if ( !is_array( $messages[ $type ] ) ) {
+            $newMessages = [];
+        }
+        if ( !empty( $messages[ $type ] ) ) {
+            $newMessages[] = [ $messages[ $type ] ];
+        }
+
+        $newMessages[] = $value;
+        $messages[ $type ] = $newMessages;
+
+        return $this->addContext( 'messages', $messages );
+    }
+
+    /**
+     * @param string $key
+     * @param mixed  $value
+     *
+     * @return $this
+     */
+    protected function addContext( $key, $value )
+    {
+        array_set( $this->context, $key, $value );
+
+        return $this;
+    }
 }
